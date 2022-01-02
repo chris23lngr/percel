526ec408281f1a5f706a35b877c132f0864a6679
chris <christoph.langer100@gmail.com>
2022-01-02 15:54:43 +0100

    improve docs

---
+++ pages/docs/js/main.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+
+  setContent('introduction');
+
+  $("div").click(function(event) {
+
+    if(!$(event.target).hasClass("example-toolbar")) {
+      return;
+    }
+
+    var parent = $(event.target).parent();
+    var expandBox = $(parent).children(".example-expandation");
+
+    console.log(event.target);
+
+    if($(event.target).hasClass("expanded")) {
+
+      $(event.target).removeClass("expanded");
+      $(expandBox).removeClass("expanded");
+
+    } else {
+
+      $(event.target).addClass("expanded");
+      $(expandBox).addClass("expanded");
+
+    }
+
+
+
+  });
+
+    $(".selector-item").click(function(event) {
+        
+      var val = $(event.target).text();
+      changeActiveStatus(event.target);
+      setContent(val);
+  
+    });
+
+  });
+  
+  function setContent(content__tag) {
+    $("#content-box").load("../contents/" + content__tag + ".html");
+  }
+  
+  function changeActiveStatus(element) {
+    $('.active').removeClass('active');
+    $(element).addClass('active');
+  }
+
+
