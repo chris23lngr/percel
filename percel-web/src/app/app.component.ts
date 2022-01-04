f682c41d531219e089b50acefb9d232467f51894
chris <christoph.langer100@gmail.com>
2022-01-04 14:03:29 +0100

    fix sidebar active issue

--- percel-web/src/app/app.component.ts
+++ percel-web/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Percel';
+
+  constructor(public _router: Router) {}
+
+  ngOnInit(): void {
+
+    if(this._router.url == "") {
+      localStorage.setItem('target', 'introduction');
+    }
+
+  }
+
 }
--- percel-web/src/app/sidebar/sidebar.component.ts
+++ percel-web/src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,9 +10,47 @@ export class SidebarComponent implements OnInit {
 
   hero = 'Windstorm';
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+
+    console.log(localStorage.getItem('target'));
+
+    var id = localStorage.getItem('target');
+
+    if(id != null) {
+
+      var element = document.getElementById(id);
+
+      if(element != null) {
+        element.classList.add('active');
+      }
+
+    }
+
+    /*
+    var x = document.getElementById("demo");   // Get the element with id="demo"
+    //x?.style.color = "red";                     // Change the color of the element
+    if(document.getElementById(id) != null) {
+      document.getElementById("demo")?.classList.add('active');
+    }
+    */
+  }
+
+  onClick(event: { target: any; srcElement: any; currentTarget: any; }) {
+    var target = event.target;
+
+    /*
+    this.router.navigate(['/' + target.textContent.toLowerCase()], { state: { item: target }});
+
+    window.history.replaceState( {}, '', `/` + target.textContent.toLowerCase());
+
+    target.classList.add('active');
+
+    this.rou
+    */
+
+    localStorage.setItem('target', target.id);
   }
 
 }
